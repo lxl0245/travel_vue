a5785692d5b2ddc52936d8efa53b3884e3f22e11
lxl0245 <lixinli.bj@163.com>
2018-06-24 12:10:42 -0700

    project init

---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span>{{msg}}</span>
+    <ul>
+      <li>List1</li>
+      <li>List2</li>
+      <li>List3</li>
+      <li>List4</li>
+      <li>List5</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
