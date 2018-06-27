cd344fbc66828a8611d4cc433eeb9bdc87d5c9f6
lxl0245 <lixinli.bj@163.com>
2018-06-27 00:44:31 -0700

    首页轮播图功能创建完成

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,19 @@
 <template>
-  <home-header></home-header>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <div>test 解决网速慢导致样式抖动的问题 </div>
+  </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of imageList" :key="item.id">
+        <img class="swiper-img"
+             :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      imageList: [
+        {id: '0001', imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/5f/171e04d556e40a02.jpg_750x200_424c4ddd.jpg'},
+        {id: '0002', imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/32/40b97c969cbad702.jpg_750x200_61fee14d.jpg'},
+        {id: '0003', imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #ffffff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26%
+    background: #ccc
+    .swiper-img
+      width: 100%
+</style>
