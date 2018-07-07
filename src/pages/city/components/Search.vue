bf6c0aa645840c09380f4a46b4d8980bef9bfbe6
lxl0245 <lixinli.bj@163.com>
2018-07-07 21:04:40 +0800

    完成：城市路由选择界面中的搜索框 的布局

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,13 +1,28 @@
 <template>
-    $END$
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Search"
-    }
+export default {
+  name: 'CitySearch'
+}
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .search
+    height:$headerHeight
+    background: $bgColor
+    padding: 0 0.1rem
+    .search-input
+      width: 100%
+      heigth: 0.6rem
+      line-height: 0.6rem
+      text-align: center
+      border-radius: $inputDefaultBorderRadius
+      // input 的边框直接卡到文字上，不美观，可以做如下优化
+      box-sizing: border-box
+      padding: 0 0.1rem
 </style>
