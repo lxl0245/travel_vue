91f1cf120b21fe3e165ba81e4acd5fe5cf2783db
lxl0245 <lixinli.bj@163.com>
2018-07-08 14:11:24 +0800

    完成： 城市选择界面的数据Ajax获取和展示逻辑

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,23 +2,48 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
 export default {
   name: 'City',
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
   components: {
     CityAlphabet,
     CityList,
     CitySearch,
     CityHeader
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.getCityInfoSucc)
+    },
+    getCityInfoSucc (res) {
+      // 把ajax接收的后台数据赋值给实例内的变量
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
