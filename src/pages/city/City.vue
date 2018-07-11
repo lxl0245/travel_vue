d50af953cf57c93d58fed4f762c8b79ccd121575
lxl0245 <lixinli.bj@163.com>
2018-07-11 10:09:21 +0800

    完成：城市列表中兄弟组件的数据传递和相关性能优化的逻辑

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,15 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hotCities="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list
+      :cities="cities"
+      :hotCities="hotCities"
+      :letter="letter"
+    ></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterChange"
+    ></city-alphabet>
   </div>
 </template>
 
@@ -18,7 +25,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   components: {
@@ -28,6 +36,9 @@ export default {
     CityHeader
   },
   methods: {
+    handleLetterChange (letter) {
+      this.letter = letter
+    },
     getCityInfo () {
       axios.get('/api/city.json')
         .then(this.getCityInfoSucc)
