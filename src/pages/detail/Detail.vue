5727d302545954c085eb2a51b8e0996324eed673
lxl0245 <lixinli.bj@163.com>
2018-07-23 21:29:42 +0800

    完成：使用递归组件实现详情页列表

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :ticketList="ticketList"></detail-list>
   </div>
 
 </template>
@@ -9,11 +10,37 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      ticketList: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三馆联票',
+              children: [
+                {title: '成人三馆联票 - 携程'},
+                {title: '成人三馆联票 - APP'}
+              ]
+
+            },
+            {title: '成人五馆联票'},
+            {title: '成人全馆通票'}
+          ]
+        },
+        {title: '学生票'},
+        {title: '儿童/老人票'},
+        {title: '热销联票'}
+      ]
+    }
   }
 }
 </script>
--- src/pages/detail/components/List.vue
+++ src/pages/detail/components/List.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
-    list
+    <div class="item"
+         v-for="(item, index) in ticketList"
+         :key="index"
+    >
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children" class="item-children  border-bottom">
+        <detail-list :ticketList="item.children"></detail-list>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DetailList'
+  name: 'DetailList',
+  props: {
+    ticketList: Array
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .item-title
+    text-align: left
+    padding: 0 0.2rem
+    .item-title-icon
+      position: relative
+      top: 0.1rem
+      display: inline-block
+      width: .4rem
+      height: .4rem
+      background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+      background-size: .4rem 3rem
+  .item-children
+    padding: 0 0.4rem
 </style>
